9ebfc0a49f1e20084b9aa86628f036c68ab20967
Pedram Ramezani <me@pedramramezani.de>
2025-12-19 15:52:19 +0100

    Added resisable panes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { SvelteFlowProvider } from '@xyflow/svelte';
 
+	import * as Resizable from '$lib/components/ui/resizable/index.js';
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
 
 	import Flow from '@/flow/Flow.svelte';
@@ -12,22 +13,25 @@
 	<!-- fitView -->
 	<!-- You need the SvelteFlowProvider so you can useSvelteFlow  -->
 	<SvelteFlowProvider>
-		<div style:width="65vw" class="max-h-screen">
-			<Flow />
-		</div>
-		<div style:width="35vw" class="border-l-2 border-zinc-200 p-8 max-h-screen overflow-y-auto">
-			<Tabs.Root value="typst">
-				<Tabs.List>
-					<Tabs.Trigger value="typst">Typst</Tabs.Trigger>
-					<Tabs.Trigger value="json">JSON</Tabs.Trigger>
-				</Tabs.List>
-				<Tabs.Content value="typst">
-					<TypstPreview />
-				</Tabs.Content>
-				<Tabs.Content value="json">
-					<JsonPreview />
-				</Tabs.Content>
-			</Tabs.Root>
-		</div>
+		<Resizable.PaneGroup direction="horizontal">
+			<Resizable.Pane defaultSize={65}>
+				<Flow />
+			</Resizable.Pane>
+			<Resizable.Handle />
+			<Resizable.Pane defaultSize={35} class="border-l-2 border-zinc-200 p-8 overflow-y-auto">
+				<Tabs.Root value="typst">
+					<Tabs.List>
+						<Tabs.Trigger value="typst">Typst</Tabs.Trigger>
+						<Tabs.Trigger value="json">JSON</Tabs.Trigger>
+					</Tabs.List>
+					<Tabs.Content value="typst">
+						<TypstPreview />
+					</Tabs.Content>
+					<Tabs.Content value="json">
+						<JsonPreview />
+					</Tabs.Content>
+				</Tabs.Root>
+			</Resizable.Pane>
+		</Resizable.PaneGroup>
 	</SvelteFlowProvider>
 </div>

c6a8a8072c903d4303d67382a5d9a847763efd02
Pedram Ramezani <pedramramezani.pr@gmail.com>
2025-12-19 15:52:43 +0100

    Added group to start node data and UI

--- src/lib/nodes/StartNode.svelte
+++ src/lib/nodes/StartNode.svelte
@@ -9,6 +9,10 @@
 
 	import Label from '@/components/ui/label/label.svelte';
 	import Input from '@/components/ui/input/input.svelte';
+	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
+
+	import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
+	import { buttonVariants } from '@/components/ui/button/index.js';
 
 	import WideHandle from '@/handles/WideHandle.svelte';
 	import NodeWrapper from './NodeWrapper.svelte';
@@ -30,17 +34,41 @@
 <NodeWrapper title="CONSORT Start" description="The flowchart will start from here.">
 	{#snippet content()}
 		<div class="flex flex-col gap-2">
-			<Label for="label">Label</Label>
-			<Input
-				name="label"
-				value={data.label}
-				type="text"
-				oninput={(evt) => {
-					const raw = (evt.target as HTMLInputElement | null)?.value ?? '';
-					updateNodeData(id, { label: raw });
-				}}
-				class="nodrag"
-			/>
+			<Collapsible.Root>
+				<div class="flex items-center justify-between space-x-4">
+					<Label for="label">Label</Label>
+					<Collapsible.Trigger
+						class={buttonVariants({ variant: 'ghost', size: 'sm', class: 'w-9 p-0' })}
+					>
+						<ChevronsUpDownIcon />
+						<span class="sr-only">Toggle</span>
+					</Collapsible.Trigger>
+				</div>
+				<Input
+					name="label"
+					value={data.label}
+					type="text"
+					oninput={(evt) => {
+						const raw = (evt.target as HTMLInputElement | null)?.value ?? '';
+						updateNodeData(id, { label: raw });
+					}}
+					class="nodrag"
+				/>
+				<Collapsible.Content class="mt-2 space-y-2">
+					<Label for="group">Group</Label>
+					<Input
+						name="group"
+						value={data.group}
+						type="text"
+						oninput={(evt) => {
+							const raw = (evt.target as HTMLInputElement | null)?.value ?? '';
+							updateNodeData(id, { group: raw });
+						}}
+						class="nodrag"
+					/>
+				</Collapsible.Content>
+			</Collapsible.Root>
+
 			<Label for="start">Population size</Label>
 			<Input
 				name="start"

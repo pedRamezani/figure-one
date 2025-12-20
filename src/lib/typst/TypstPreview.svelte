5b883c72a07234af7ae375e53948db316745b3b3
Pedram Ramezani <pedramramezani.pr@gmail.com>
2025-12-20 12:34:22 +0100

    Added SVG export button and functionality.

--- src/lib/typst/TypstPreview.svelte
+++ src/lib/typst/TypstPreview.svelte
@@ -6,6 +6,8 @@
 
 	import { convertFlowchartToTypstJson } from './index.ts';
 
+	import { Button } from '@/components/ui/button/index.js';
+
 	const { toObject } = useSvelteFlow();
 	const jsonData = $derived(convertFlowchartToTypstJson(toObject()));
 	const encoder = new TextEncoder();
@@ -17,8 +19,48 @@
 			source = await fetch('figure1.typ').then((response) => response.text());
 		})();
 	});
+
+	let currentSvg: string | undefined = $state();
+	const exportSvg = (mainContent: string | undefined) => {
+		if (!mainContent) return;
+
+		const blob = new Blob([mainContent], { type: 'image/svg+xml' });
+
+		// Creates element with <a> tag
+		const link = document.createElement('a');
+
+		// Sets file content in the object URL
+		link.href = URL.createObjectURL(blob);
+
+		// Sets file name
+		link.download = 'flowchart.svg';
+
+		// Triggers a click event to <a> tag to save file.
+		// document.body.appendChild(link);
+		link.click();
+		// document.body.removeChild(link);
+		URL.revokeObjectURL(link.href);
+	};
 </script>
 
-<h2 class="font-bold text-xl mb-2">Typst preview</h2>
+<div class="flex flex-col h-full">
+	<h2 class="font-bold text-xl mb-2">Typst preview</h2>
 
-<TypstDocument {source} sourceShadowMappings={{ '/assets/flowchart.json': encodedJsonData }} />
+	<TypstDocument
+		{source}
+		sourceShadowMappings={{ '/assets/flowchart.json': encodedJsonData }}
+		onSvgChange={(svg) => {
+			currentSvg = svg;
+		}}
+		class="grow"
+	/>
+
+	<Button
+		variant="outline"
+		class="mt-4 self-end"
+		disabled={!currentSvg}
+		onclick={() => {
+			exportSvg(currentSvg);
+		}}>Download SVG</Button
+	>
+</div>

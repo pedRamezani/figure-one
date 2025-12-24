3f0d77b4cf510fbb120dae942d7a1c025d55a7a4
Pedram Ramezani <me@pedramramezani.de>
2025-12-24 13:46:54 +0100

    Added import/export JSON button. TODO: Add import JSON functionality.

--- src/lib/typst/JsonPreview.svelte
+++ src/lib/typst/JsonPreview.svelte
@@ -2,6 +2,34 @@
 	import { useSvelteFlow } from '@xyflow/svelte';
 	import { convertFlowchartToTypstJson } from './index.ts';
 
+	import * as ButtonGroup from '$lib/components/ui/button-group/index.js';
+	import Button from '@/components/ui/button/button.svelte';
+
+	import { downloadBlob } from './index.ts';
+
+	function isValidJSON(file: File) {
+		// TODO
+	}
+
+	function importJSON(): void {
+		const fileInput = document.createElement('input');
+		fileInput.type = 'file';
+		fileInput.onchange = (event) => {
+			const target = event.target as HTMLInputElement | null;
+			if (target === null) return;
+
+			const file = target.files ? target.files[0] : null;
+			if (file === null) return;
+
+			console.log(file.name);
+		};
+		fileInput.click();
+	}
+
+	function exportJSON(): void {
+		downloadBlob(flowchartStringified, 'application/json', 'flowchart.json');
+	}
+
 	// JSON encode
 	const { toObject } = useSvelteFlow();
 
@@ -12,5 +40,11 @@
 	});
 </script>
 
-<h2 class="font-bold text-xl mb-2">JSON preview</h2>
-<pre class="overflow-y-auto">{flowchartStringified}</pre>
+<div class="flex flex-col h-full">
+	<h2 class="font-bold text-xl mb-2">JSON preview</h2>
+	<pre class="overflow-y-auto grow">{flowchartStringified}</pre>
+	<ButtonGroup.Root class="self-end" aria-label="Download options">
+		<Button class="self-end" variant="outline" onclick={importJSON}>Import JSON</Button>
+		<Button class="self-end" variant="outline" onclick={exportJSON}>Export JSON</Button>
+	</ButtonGroup.Root>
+</div>

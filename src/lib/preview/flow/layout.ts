2f2cf2eba49e9d2baeb2dbab384a7ccffa4f8d5d
Pedram Ramezani <pedramramezani.pr@gmail.com>
2026-03-12 12:32:53 +0100

    Fixed group node alignment for group nodes connected to rows

--- src/lib/preview/flow/layout.ts
+++ src/lib/preview/flow/layout.ts
@@ -1,7 +1,7 @@
 import Dagre from '@dagrejs/dagre';
 import type { Node, Edge } from '@xyflow/svelte';
 
-import { substepTarget, groupSource, type RegisteredNodeType } from '@/nodes/types';
+import { substepTarget, groupSource, rowTargetGroup, type RegisteredNodeType } from '@/nodes/types';
 
 export function getLayoutedElements(
 	nodes: Node[],
@@ -15,12 +15,19 @@ export function getLayoutedElements(
 	g.setGraph({ rankdir: 'TB', nodesep: gap, ranksep: gap });
 
 	// Exclude substep and group nodes/edges from Dagre so we can layout them manually
-	const excludedNodeTypes = new Set([substepTarget.nodeType, groupSource.nodeType]);
+	// Also exclude row nodes
+	const excludedNodeTypes = new Set([
+		substepTarget.nodeType,
+		groupSource.nodeType,
+		rowTargetGroup.nodeType
+	]);
 
 	edges
 		.filter(
 			(edge) =>
-				edge.targetHandle !== substepTarget.handleId && edge.sourceHandle !== groupSource.handleId
+				edge.targetHandle !== substepTarget.handleId &&
+				edge.sourceHandle !== groupSource.handleId &&
+				edge.targetHandle != rowTargetGroup.handleId
 		)
 		// .filter((edge) => {
 		// 	const src = nodes.find((n) => n.id === edge.source);
@@ -48,6 +55,8 @@ export function getLayoutedElements(
 			const [anchorX, anchorY] = node.origin ?? [0, 0];
 			// We are shifting the dagre node position (anchor=center center) to the anchor
 			// so it matches the Svelte Flow node anchor point (default: top left).
+			// Theoretically we also have to adjust this to be relative coordinates for child nodes
+			// We can mitigate this simply by setting the position of the parent node to the (0,0).
 			const x = position.x + (anchorX - 0.5) * (node.measured?.width ?? 0);
 			const y = position.y + (anchorY - 0.5) * (node.measured?.height ?? 0);
 
@@ -102,6 +111,8 @@ export function getLayoutedElements(
 			};
 		});
 
+	const rawRowNodes = nodes.filter((n) => n.type === rowTargetGroup.nodeType);
+
 	// Manually layout group nodes to the left of their connected start/step nodes.
 	const groupNodes = nodes
 		.filter((n) => n.type === groupSource.nodeType)
@@ -111,15 +122,24 @@ export function getLayoutedElements(
 				return { ...gNode, position: { x: 0, y: 0 } } as Node;
 			}
 
+			const rowTargetNodesIds = connections
+				.map((tid) => rawRowNodes.find((n) => n.id === tid))
+				.filter((n): n is Node => !!n)
+				.map((n) => n.id);
+
+			const connectedToRow = rowTargetNodesIds.length > 0;
+
 			// find target nodes in tbNodes (they were laid out by Dagre)
-			const targetNodes = connections
-				.map((tid) => tbNodes.find((n) => n.id === tid))
+			const tbTargetNodes = connections
+				.map((tid) =>
+					tbNodes.find((n) => n.id === tid || rowTargetNodesIds.includes(n.parentId ?? ''))
+				)
 				.filter((n): n is Node => !!n);
 
-			if (targetNodes.length === 0) return { ...gNode, position: { x: 0, y: 0 } } as Node;
+			if (tbTargetNodes.length === 0) return { ...gNode, position: { x: 0, y: 0 } } as Node;
 
-			const topYs = targetNodes.map((t) => t.position.y);
-			const bottomYs = targetNodes.map(
+			const topYs = tbTargetNodes.map((t) => t.position.y);
+			const bottomYs = tbTargetNodes.map(
 				(t) => t.position.y + (1 - (t.origin?.[1] ?? 0)) * (t.measured?.height ?? 0)
 			);
 			const minTop = Math.min(...topYs);
@@ -129,18 +149,29 @@ export function getLayoutedElements(
 			// This only works because the origin is at Position.Right
 			// Not robust, but works for now.
 			const minLeft = Math.min(
-				...targetNodes.map((t) => t.position.x - (t.origin?.[0] ?? 0) * (t.measured?.width ?? 0))
+				...tbTargetNodes.map((t) => t.position.x - (t.origin?.[0] ?? 0) * (t.measured?.width ?? 0))
 			);
 
 			// Use 3x gap
-			const x = minLeft - gap * (targetNodes.length <= 1 ? 1 : 3);
+			// TODO: Fix origin similar to above: anchorX * (ssNode.measured?.width ?? 0)
+			// Then remove the hard coded origin override
+			const x = minLeft - gap * (tbTargetNodes.length <= 1 ? 1 : 3) - (connectedToRow ? 20 : 0);
 			const y = centerY;
 
-			return { ...gNode, position: { x, y }, origin: gNode.origin } as Node;
+			return { ...gNode, position: { x, y }, origin: [1, 0.5] } as Node;
 		});
 
+	// Add back row nodes for now
+	const rowNodes = rawRowNodes.map((n) => ({
+		...n,
+		// This will be adjusted by the row nodes internal logic anyway,
+		// which will be doing the final layouting for us.
+		position: { x: 0, y: 0 }
+	}));
+
 	return {
-		nodes: [...tbNodes, ...groupNodes, ...substepNodes],
+		// Parent nodes have to come first
+		nodes: [...rowNodes, ...tbNodes, ...groupNodes, ...substepNodes],
 		edges
 	};
 }

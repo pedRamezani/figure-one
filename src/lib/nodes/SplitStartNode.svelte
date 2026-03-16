1a4c62f9b6a6d5a10b4e2dde61932d438c2f9bef
Pedram Ramezani <pedramramezani.pr@gmail.com>
2026-03-16 19:42:45 +0100

    Added functional number field for all nodes except stepnodes

--- src/lib/nodes/SplitStartNode.svelte
+++ src/lib/nodes/SplitStartNode.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { useSvelteFlow, useNodeConnections, type NodeProps } from '@xyflow/svelte';
 
-	import Label from '@/components/ui/label/label.svelte';
 	import Input from '@/components/ui/input/input.svelte';
+	import Label from '@/components/ui/label/label.svelte';
+	import * as NumberField from '$lib/components/ui/number-field';
 
 	import NodeWrapper from './NodeWrapper.svelte';
 
@@ -32,6 +33,15 @@
 			updateNodeData(id, { row: 0 });
 		}
 	});
+
+	// Split population size effect
+	let splitPopulationSize = $state(data.value as number);
+	$effect(() => {
+		const parsedValue = Number.isFinite(splitPopulationSize) ? splitPopulationSize : 0;
+		if (data.value !== parsedValue) {
+			updateNodeData(id, { value: parsedValue });
+		}
+	});
 </script>
 
 <NodeWrapper
@@ -56,17 +66,13 @@
 
 			<!-- Main Section -->
 			<Label for="start">Split population size</Label>
-			<Input
-				name="start"
-				value={data.value}
-				type="number"
-				oninput={(evt) => {
-					const raw = (evt.target as HTMLInputElement | null)?.value ?? '';
-					const parsedValue = Number.isFinite(Number(raw)) && raw !== '' ? parseInt(raw, 10) : 0;
-					updateNodeData(id, { value: parsedValue });
-				}}
-				class="nodrag"
-			/>
+			<NumberField.Root min={0} bind:value={splitPopulationSize}>
+				<NumberField.Group class="nodrag">
+					<NumberField.Decrement />
+					<NumberField.Input class="w-[10ch]" name="start" />
+					<NumberField.Increment />
+				</NumberField.Group>
+			</NumberField.Root>
 		</div>
 	{/snippet}
 </NodeWrapper>

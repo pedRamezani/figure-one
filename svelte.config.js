1f4fefcea863f1a41b1903be45460c31d8094cf1
Pedram Ramezani <me@pedramramezani.de>
2026-03-28 19:18:21 +0100

    Revert "Merge pull request #4 from pedRamezani/cloudflare/workers-autoconfig"
    This reverts commit 64cce6f0a17a04b0573c891bb9e2a9676f4a0ecb, reversing
    changes made to 0684bfaf2f4950731b4ea95d2cff8c070f486430.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,26 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter(), alias: { '@/*': './src/lib/*' } }
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			'@/*': './src/lib/*'
+		},
+		// typescript: {
+		// 	config: (config) => {
+		// 		config["include"] = [
+		// 			...config["include"],
+		// 			"../src/lib/components/ui/color-picker/colorpicker.d.ts"
+		// 		]
+
+		// 		return config
+		// 	}
+		// }
+	}
 };
 
 export default config;
